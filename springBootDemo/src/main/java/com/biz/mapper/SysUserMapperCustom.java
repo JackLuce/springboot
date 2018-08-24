c67c3d4d049665487166dcd88f14a97d52b98007
JackLuce <2357714018@qq.com>
2018-08-24 17:14:17 +0800

    dev-mapper注释

--- springBootDemo/src/main/java/com/biz/mapper/SysUserMapperCustom.java
+++ springBootDemo/src/main/java/com/biz/mapper/SysUserMapperCustom.java
@@ -4,7 +4,9 @@ import com.biz.pojo.SysUser;
 import com.biz.utils.MyMapper;
 
 import java.util.List;
-
+/**
+自定义mapper映射
+*/
 public interface SysUserMapperCustom extends MyMapper<SysUser> {
 
     public List<SysUser> findUserSimpleInfoById(String userId);
